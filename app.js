ce403c440b079c2f772c908d3de150183a15c7e9
meilixiaozhang <redpangci@gmail.com>
2020-04-11 22:54:01 +1000

    delete items from custom todo lists

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -61,7 +62,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/:customListName", function(req,res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name:customListName}, function(err, foundList){
     if (!err) {
@@ -108,13 +109,23 @@ app.post("/", function(req, res){
 
 app.post("/delete", function(req,res){
   const checkedItemId = req.body.checkBox;
-  // console.log(checkedItemId);
-  Item.findByIdAndRemove(checkedItemId, function(err){
-    if (!err) {
-      console.log("Successfully deleted");
-      res.redirect("/")
-    }
-  })
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if (!err) {
+        console.log("Successfully deleted");
+        res.redirect("/")
+      }
+    })
+  } else {
+    List.findOneAndUpdate({name: listName},{$pull: {items: {_id: checkedItemId}}},function(err, foundList){
+      if (!err){
+        res.redirect("/"+listName)
+      }
+    })
+  }
+
 });
 
 app.get("/about", function(req, res){
