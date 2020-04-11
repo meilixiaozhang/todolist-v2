7e9cf9a21d0e171d5e849f569313c3dd60f3508a
meilixiaozhang <redpangci@gmail.com>
2020-04-11 20:11:30 +1000

    render database items

--- app.js
+++ app.js
@@ -34,18 +34,24 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Success");
-  }
-})
+
 
 app.get("/", function(req, res) {
 
-
-  res.render("list", {listTitle: "Today", newListItems: items});
+  Item.find({}, function(err, foundItems){
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      })
+      res.redirect("/")
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+  })
 
 });
 
